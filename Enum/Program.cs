260bf92f8a4274884d1c0ad164034f6c75a76056
Furkan Yılmaz <furkanyilmaz870@gmail.com>
2022-08-13 00:28:28 +0300

    Add project files.

---
+++ Enum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EnumDersi
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Günler.Cumartesi);
+            Console.WriteLine((int)Günler.Cumartesi);
+
+            int sıcaklık = Convert.ToInt32(Console.ReadLine());
+
+            if (sıcaklık < (int)HavaDurumu.normal)
+                Console.WriteLine("Havanın ısınmasını bekleyelim.");
+            else if (sıcaklık >= (int)HavaDurumu.sıcak)
+                Console.WriteLine("Havanın biraz soğumasını bekleyelim.");
+            else if (sıcaklık < (int)HavaDurumu.sıcak && sıcaklık > (int)HavaDurumu.normal)
+                Console.WriteLine("Hadi dışarı çıkalım!");
+
+        }
+    }
+
+    enum Günler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=26,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        soğuk=5,
+        normal=15,
+        sıcak=30
+    }
+}
+
